1b543672aaa900eb3c330e3ede35f205dc90e20a
Gilberto González Rodríguez <47121318+ginrod@users.noreply.github.com>
2025-08-06 23:41:15 -0400

    add basic API for files (wip) (#2)

---
+++ InstaShare.API/InstaShare.API/Dtos/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace InstaShare.API.Dtos
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public int? TotalRecords { get; set; }
+
+        public ApiResponse(bool success, string message, T data, int? totalRecords = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+            TotalRecords = totalRecords;
+        }
+    }
+}
---
+++ InstaShare.API/InstaShare.API/FileEntity.cs
@@ -0,0 +1,9 @@
+namespace InstaShare.API
+{
+    public class FileEntity
+    {
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+        public double Size { get; set; }
+    }
+}
