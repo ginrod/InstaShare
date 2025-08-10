f3ee2351315a623b6ab85b09955649a4a0080937
ginrod <gginrod20@gmail.com>
2025-08-10 11:22:33 +0100

    add middleware and envs

--- InstaShare/InstaShare.API/Program.cs
+++ InstaShare/InstaShare.API/Program.cs
@@ -8,8 +8,41 @@ using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using InstaShare.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options);
+                options.Events = new JwtBearerEvents();
+
+                /// <summary>
+                /// Below you can do extended token validation and check for additional claims, such as:
+                ///
+                /// - check if the caller's tenant is in the allowed tenants list via the 'tid' claim (for multi-tenant applications)
+                /// - check if the caller's account is homed or guest via the 'acct' optional claim
+                /// - check if the caller belongs to right roles or groups via the 'roles' or 'groups' claim, respectively
+                ///
+                /// Bear in mind that you can do any of the above checks within the individual routes and/or controllers as well.
+                /// For more information, visit: https://docs.microsoft.com/azure/active-directory/develop/access-tokens#validate-the-user-has-permission-to-access-this-data
+                /// </summary>
+
+                //options.Events.OnTokenValidated = async context =>
+                //{
+                //    string[] allowedClientApps = { /* list of client ids to allow */ };
+
+                //    string clientappId = context?.Principal?.Claims
+                //        .FirstOrDefault(x => x.Type == "azp" || x.Type == "appid")?.Value;
+
+                //    if (!allowedClientApps.Contains(clientappId))
+                //    {
+                //        throw new System.Exception("This client is not authorized");
+                //    }
+                //};
+            }, options => { builder.Configuration.Bind("AzureAd", options); });
 
 builder.Services.AddCors(options =>
 {
