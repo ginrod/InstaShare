ebc6c5af12ea5e04150267a3b37a9c6cedba6883
ginrod <gginrod20@gmail.com>
2025-08-10 15:55:33 +0100

    clean AppDbContext and add OnModelCreating

--- InstaShare/InstaShare.Infrastructure/AppDbContext.cs
+++ InstaShare/InstaShare.Infrastructure/AppDbContext.cs
@@ -3,56 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InstaShare.Infrastructure;
 
-public class AppDbContext : DbContext, IAppDbContext
+public class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
+    public DbSet<FileEntity> Files => Set<FileEntity>();
 
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
-
-
-    public DbSet<FileEntity> Files { get; set; } = null!;
+    public AppDbContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<FileEntity>();
-
-        modelBuilder.Entity<FileEntity>()
-            .HasData(
-                new FileEntity
-                {
-                    Id = new Guid("ff0c022e-1aff-4ad8-2231-08db0378ac98"),
-                    Name = "Default File",
-                    Status = "upload",
-                    Size = 1024
-                }
-            );
-    }
-
-    // Basic Code to add more sample data
-    // It creates 500 Random Files
-    public void SeedInitialData()
-    {
-        var files = new List<FileEntity>();
-
-        for (int i = 1; i <= 500; ++i)
+        modelBuilder.Entity<FileEntity>(e =>
         {
-            var file = new FileEntity
-            {
-                Id = Guid.NewGuid(),
-                Name = $"File {i}",
-                Status = "upload",
-                Size = Random.Shared.Next(1, 51200),
-            };
-
-            files.Add(file);
-        }
-
-        Files.AddRange(files);
+            e.Property(p => p.Status).HasConversion<byte>();
+            e.HasIndex(p => new { p.UserId, p.HashSha256 }).IsUnique();
+            e.HasIndex(p => p.UserId);
+            e.HasIndex(p => p.Status);
+        });
     }
 }
